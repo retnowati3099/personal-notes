56c0a7410b0c4ef8627b6d1989362f11abeddd13
Retno Wati <retnowati3099@gmail.com>
2023-10-10 17:08:26 +0700

    made event handler for each input (included buat button) and prepared the state

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -4,38 +4,54 @@ export default class NoteInput extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { title: "", remaining: 50 };
+    this.state = { title: "", body: "", remaining: 50 };
 
-    this.onTitleChangeHandler = this.onTitleChangeHandler.bind(this);
+    this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
+    this.onBodyChangeEventHandler = this.onBodyChangeEventandler.bind(this);
+    this.OnSubmitEventHandler = this.OnSubmitEventHandler.bind(this);
   }
 
-  onTitleChangeHandler(event) {
-    const title = event.target.value;
-    this.setState({ title });
+  onTitleChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        title: event.target.value,
+      };
+    });
   }
+
+  onBodyChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        body: event.target.value,
+      };
+    });
+  }
+
+  OnSubmitEventHandler(event) {
+    event.preventDefault();
+    this.props.addNode(this.state);
+  }
+
   render() {
     const { title, remaining } = this.state;
     const remain = remaining - title.length;
 
     return (
-      <div className="note-input">
-        <form className="note-input">
-          <p className="note-input__title__char-limit">
-            Sisa karakter: {remain}
-          </p>
-          <input
-            type="text"
-            placeholder="Ini adalah judul ..."
-            className="note-input__title "
-            onChange={this.onTitleChangeHandler}
-          />
-          <textarea
-            placeholder="Tuliskan catatanmu di sini ..."
-            className="note-input__body"
-          ></textarea>
-          <button type="submit">Buat</button>
-        </form>
-      </div>
+      <form className="note-input" onSubmit={this.OnSubmitEventHandler}>
+        <p className="note-input__title__char-limit">Sisa karakter: {remain}</p>
+        <input
+          type="text"
+          placeholder="Ini adalah judul ..."
+          className="note-input__title "
+          onChange={this.onTitleChangeEventHandler}
+        />
+        <textarea
+          placeholder="Tuliskan catatanmu di sini ..."
+          className="note-input__body"
+          onChange={this.onBodyChangeEventHandler}
+        ></textarea>
+        <button type="submit">Buat</button>
+      </form>
     );
   }
 }
